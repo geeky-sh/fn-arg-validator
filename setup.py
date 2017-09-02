2177470e6a2ed4a25e52489904ed4a1408d832f4
geeky-sh <aash.discover@gmail.com>
2017-09-02 18:50:31 +0530

    updating version

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='fn-arg-validator',
-    version='0.11',
+    version='0.12',
     description="Provides a decorates which validates the arguments passed to a function",
     author="aash",
     author_email="aash.discover@gmail.com",
-    py_modules=['validator'],
+    py_modules=['fn_validator'],
 )
